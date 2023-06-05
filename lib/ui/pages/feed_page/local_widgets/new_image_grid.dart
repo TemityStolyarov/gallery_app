093632d1c53d07602fd709994d09c0c3c5421117
TemityStolyarov <127617131+TemityStolyarov@users.noreply.github.com>
2023-06-06 02:11:17 +0300

    added pull-to-refresh feature
    added pull-to-refresh feature

--- lib/ui/pages/feed_page/local_widgets/new_image_grid.dart
+++ lib/ui/pages/feed_page/local_widgets/new_image_grid.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:gallery_app/api/gallery_api.dart';
 import 'package:gallery_app/api/image_model.dart';
+import 'package:gallery_app/core/constants.dart';
 import 'package:gallery_app/ui/pages/feed_page/local_widgets/image_detail_page.dart';
 
 import 'package:http/http.dart' as http;
@@ -24,6 +25,18 @@ class _ImageGridScreenState extends State<NewImageGridScreen> {
     _imagesFuture = _galleryApi.getNewImages();
   }
 
+  Future<void> _refreshPage() async {
+    final response = await _galleryApi.getNewImages();
+    setState(() {
+      _imagesFuture = Future.delayed(
+        Duration.zero,
+        () {
+          return response;
+        },
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<ImageModel>>(
@@ -34,46 +47,53 @@ class _ImageGridScreenState extends State<NewImageGridScreen> {
           final screenHeight = MediaQuery.of(context).size.height - 258;
           return SizedBox(
             height: screenHeight,
-            child: SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.manual,
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 150 / 100,
-                ),
-                itemCount: images.length,
-                clipBehavior: Clip.hardEdge,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () async {
-                      final response = await http.get(Uri.parse(
-                          'https://gallery.prod1.webant.ru${images[index].user}'));
-                      final jsonResponse = jsonDecode(response.body);
-                      final username = jsonResponse['username'];
+            child: RefreshIndicator(
+              color: DEFAULT_ACCENT_COLOR,
+              onRefresh: _refreshPage,
+              displacement: 0,
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.manual,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 150 / 100,
+                  ),
+                  itemCount: images.length,
+                  clipBehavior: Clip.hardEdge,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () async {
+                        final response = await http.get(Uri.parse(
+                            'https://gallery.prod1.webant.ru${images[index].user}'));
+                        final jsonResponse = jsonDecode(response.body);
+                        final username = jsonResponse['username'];
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ImageDetailPage(
-                            images[index],
-                            username: username,
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ImageDetailPage(
+                              images[index],
+                              username: username,
+                            ),
                           ),
+                        );
+                      },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image.network(
+                          'https://gallery.prod1.webant.ru/media/${images[index].image.name}',
+                          fit: BoxFit.cover,
                         ),
-                      );
-                    },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5.0),
-                      child: Image.network(
-                        'https://gallery.prod1.webant.ru/media/${images[index].image.name}',
-                        fit: BoxFit.cover,
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           );
